543eeff57182bae5376c61d34927c06c46922340
Nanomotion <der.plays.mc@gmail.com>
2018-05-20 10:22:30 -0400

    New site!1!11!

---
+++ assets/prices.js
@@ -0,0 +1,16 @@
+function calcPrice() {
+    var price = 0;
+    if ($("#landing").is(":checked")) {
+        price += 10;
+    } if ($("#cmds").is(":checked")) {
+        price += 7;
+    } if ($("#about").is(":checked")) {
+        price += 5;
+    } if ($("#404").is(":checked")) {
+        price += 3;
+    } if ($("#template").is(":checked")) {
+       price += 7;
+   }
+   price += $("#extra").val() * 5;
+   $("#price").html("~$" + price);
+}
